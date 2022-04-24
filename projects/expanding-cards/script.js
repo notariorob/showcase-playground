4c5c2f3e6f0656510824508229bfdabf8ff36a2a
Roberto Notario <notariorob@gmail.com>
2022-04-24 12:36:26 +0100

    feat: add expanding cards

---
+++ projects/expanding-cards/script.js
@@ -0,0 +1,10 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => card.addEventListener('click', () => {
+  removeActiveClass();
+  card.classList.add('active');
+}))
+
+const removeActiveClass = () => {
+  cards.forEach(card => card.classList.remove('active'));
+}
